e1d3ff662e85dfdf4a043c5a590debf7275544ec
Düzgün <cduezguen@init.de>
2019-02-18 13:43:09 +0100

    bild und überschrift in chrome logo geändert

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,10 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   var mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
+  if(mySrc === 'images/chrome-icon.png') {
+    myImage.setAttribute ('src','images/chrome-icon.png');
   } else {
-    myImage.setAttribute ('src','images/firefox-icon.png');
+    myImage.setAttribute ('src','images/chrome-icon.png');
   }
 }
 
@@ -17,16 +17,16 @@ var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
 
 function setUserName() {
-  var myName = prompt('Please enter your name.');
+  var myName = prompt('Bitte gib deinen namen ein.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  myHeading.innerHTML = 'CHROME is cool, ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'Chrome ist Cool, ' + storedName;
 }
 
 myButton.onclick = function() {
